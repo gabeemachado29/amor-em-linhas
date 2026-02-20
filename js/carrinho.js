b49e424bdb89d05beefc9ac0fe7094ea6e144b84
Gabriel Machado França <127861414+gabeemachado29@users.noreply.github.com>
2026-02-20 14:56:02 -0300

    Update carrinho.js

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,6 +1,31 @@
-function addCarrinho(id){
-    let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
-    carrinho.push(id);
+function getCarrinho(){
+    return JSON.parse(localStorage.getItem("carrinho")) || {};
+}
+
+function salvarCarrinho(carrinho){
     localStorage.setItem("carrinho", JSON.stringify(carrinho));
-    alert("Produto adicionado!");
+}
+
+function addCarrinho(id){
+    let carrinho = getCarrinho();
+
+    if(carrinho[id]){
+        carrinho[id]++;
+    }else{
+        carrinho[id] = 1;
+    }
+
+    salvarCarrinho(carrinho);
+    alert("Produto adicionado ao carrinho!");
+}
+
+function removerItem(id){
+    let carrinho = getCarrinho();
+    delete carrinho[id];
+    salvarCarrinho(carrinho);
+    location.reload();
+}
+
+function limparCarrinho(){
+    localStorage.removeItem("carrinho");
 }
