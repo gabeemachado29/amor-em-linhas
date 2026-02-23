478a1efae15971637d497bcaf7ffbb67d1951d01
Gabriel Machado França <127861414+gabeemachado29@users.noreply.github.com>
2026-02-23 08:47:22 -0300

    Update historico.php

--- historico.php
+++ historico.php
@@ -20,6 +20,34 @@ $pedidos = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <title>Histórico</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+    const temaSalvo = localStorage.getItem("tema");
+
+    if (temaSalvo === "escuro") {
+        document.body.classList.add("bg-dark", "text-light");
+        document.querySelectorAll(".card").forEach(card => {
+            card.classList.add("bg-secondary", "text-light");
+        });
+        document.querySelector(".navbar").classList.remove("navbar-dark","bg-dark");
+        document.querySelector(".navbar").classList.add("navbar-dark","bg-black");
+    }
+});
+
+function alternarTema() {
+    const escuro = document.body.classList.toggle("bg-dark");
+
+    if (escuro) {
+        localStorage.setItem("tema","escuro");
+        document.body.classList.add("text-light");
+    } else {
+        localStorage.setItem("tema","claro");
+        document.body.classList.remove("text-light");
+    }
+
+    location.reload();
+}
+</script>
 <body>
 
 <h2>Meus Pedidos</h2>
