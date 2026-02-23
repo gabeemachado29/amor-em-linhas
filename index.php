26a2addf3b0bcd21314eb1e2b09575530400e3c1
Gabriel Machado França <127861414+gabeemachado29@users.noreply.github.com>
2026-02-23 09:13:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -15,15 +15,16 @@ session_start();
 <?php include "includes/navbar.php"; ?>
 
 <div class="container mt-5">
-    <h2>Produtos</h2>
+    <h2 class="mb-4">Nossos Produtos</h2>
 
     <div class="row">
+
         <div class="col-md-4 mb-4">
             <div class="card shadow-sm">
                 <img src="https://via.placeholder.com/300x250" class="card-img-top">
                 <div class="card-body">
-                    <h5>Vestido Floral</h5>
-                    <p>R$ 129,90</p>
+                    <h5 class="card-title">Vestido Floral</h5>
+                    <p class="card-text">R$ 129,90</p>
                     <a href="carrinho.php?add=1&nome=Vestido Floral&preco=129.90"
                        class="btn btn-primary w-100">
                         Adicionar
@@ -31,6 +32,7 @@ session_start();
                 </div>
             </div>
         </div>
+
     </div>
 </div>
 
